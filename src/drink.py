7c6a97a4849f19b2a8fd3cdc0eadfa8eacb9c490
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-04-06 12:57:09 +0100

    Add source code for drink

---
+++ src/drink.py
@@ -0,0 +1,6 @@
+class Drink:
+    def __init__(self, input_name, input_price, input_stock):
+        self.name = input_name
+        self.price = input_price
+        self.stock = input_stock 
+        
