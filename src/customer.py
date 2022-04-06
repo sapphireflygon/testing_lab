7cfaee12acd36bb938143dbface4b2e614cae268
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-04-06 13:47:41 +0100

    Add function to remove money from a customer

--- src/customer.py
+++ src/customer.py
@@ -2,4 +2,7 @@ class Customer:
     def __init__(self, input_name, input_wallet):
         self.name = input_name
         self.wallet = input_wallet
-        
+    
+    def remove_money_from_customer(self, amount):
+        self.wallet -= amount
+    
