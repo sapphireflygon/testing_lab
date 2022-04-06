c0f3b428d97ca859905f13715e5074abf8730759
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-04-06 13:01:07 +0100

    Add source code for pub

---
+++ src/pub.py
@@ -0,0 +1,7 @@
+class Pub:
+    def __init__(self, input_name, input_till, input_drinks_list):
+        self.name = input_name
+        self.till = input_till
+        self.drinks_list = input_drinks_list
+
+    
